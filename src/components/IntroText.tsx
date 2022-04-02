ff67820935770d5fe1983890157ec6606d21c8bb
JahnoelRondon <jahnoelrondon03@gmail.com>
2022-04-02 17:52:09 -0400

    styled intro description component

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import './componentStyles.css'
+
 
 export const IntroText = () => {
   return (
-    <section>
-      <p>
+    <section className='intro'>
+      <p className='intro__text'>
         This website is to help you learn and discover the meaning behind the classic tarot cards. 
         You may click on the cards at any time to see a full description and search to filter for a specifc one.
       </p>
+      <p className='intro__text2'>Choose one of the classic 78 Rider Waite Cards.</p>
     </section>
   )
 }
