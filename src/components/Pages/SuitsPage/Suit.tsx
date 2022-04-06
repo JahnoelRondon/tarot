8a372586b725c70ffafc47dae289445b68ab0f58
JahnoelRondon <jahnoelrondon03@gmail.com>
2022-04-05 20:30:20 -0400

    completed description for suits as well as its styling

--- src/components/Pages/SuitsPage/Suit.tsx
+++ src/components/Pages/SuitsPage/Suit.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Card} from '../../../model'
 import {CardList} from '../../Cards/CardList'
 import { suit } from '../../../model'
+import './suit.css'
 
 interface Props {
   cards: Card[] | undefined
@@ -15,7 +16,10 @@ export const Suit = ({cards}: Props) => {
   return (
     <section className='suitPage'>
       {/* make a component that renders information based on the suit name, can use cards[0].suit to compare */}
-      <p>{cards ? suit[suitString as keyof typeof suit]  : ''}</p>
+      <div className='suitPage_info'>
+        <h2 className='suitPage_title'>The {suitString} cards and what they mean.</h2>
+        <p className='suitPage_txt'>{cards ? suit[suitString as keyof typeof suit] : ''}</p>
+      </div>
 
       {/* cardlist components */}
       <CardList cardsListed={cards}/>
--- src/model.ts
+++ src/model.ts
@@ -33,9 +33,31 @@ interface Suits {
 }
 
 export let suit: Suits = {
-  trump: 'trumps are',
-  cups: 'cups are',
-  swords: 'swords are',
-  wands: 'wands are',
-  pentacles: 'pentacles are',
+  trump: `
+    The Trumps cards (also known as Major Arcana) signify "above all". The Major Arcana cards are not like the
+     Minors—while the Minors can be pretty specific (focusing on actual situations, feelings, etcetera), the 
+     Majors are more about energy fields and influences. They may reflect a spiritual search, attainment or 
+     life lesson. Sometimes they reflect forces beyond our control. They warn us that resistance is futile, and 
+     that we need to adjust our perception or increase understanding and acceptance.
+  `,
+  cups: `
+    The Cups are represented as clergymen or statesmen.
+    Cups are connected to emotions, and relationships with others and self.
+  `
+    ,
+  swords: `
+    The Swords are represented by the noblility or persons who held their rank by military service.
+    Swords are connected to the intellect and communication.
+  `
+    ,
+  wands: `
+    The Wands or (Clubs) are represented by the peasants and working class.
+    The Wands are connected to inspiration and action.
+  `
+    ,
+  pentacles: `
+    The Pentacles or (Coins) represented tradesmen and the industrial class.
+    Pentacles are connected to the material world, work, health, money.
+  `
+    ,
 }
